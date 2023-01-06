e2936d0d7fd3c8fc26677923cbeb9461ea9a45c0
Tomáš Trejdl <trejdl.tomas@gmail.com>
2023-01-06 12:13:43 +0100

    Wrap callout emoji in span

--- src/utils/md.ts
+++ src/utils/md.ts
@@ -57,10 +57,9 @@ export const callout = (text: string, icon?: CalloutIcon) => {
   }
 
   // the replace is done to handle multiple lines
-  return `<aside> ${emoji ? emoji + ' ' : ''}${text.replace(
-    /\n/g,
-    '  \n '
-  )}</aside>`
+  return `<aside>${
+    emoji ? `<span class="callout-emoji">${emoji}</span>` : ''
+  } <p>${text}</p></aside>`
 };
 
 export const bullet = (text: string, count?: number) => {
